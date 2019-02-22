c779ff90cb178d3998b37ad163264d452f67a83b
Conor Fearon <CFearon96@gmail.com>
2019-02-22 20:18:15 +0000

    Added React Frontend --Basic frontend display

---
+++ client/src/components/launches.js
@@ -0,0 +1,41 @@
+import React, { Component, Fragment } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import LaunchItem from "./launchitem";
+
+const launchQuery = gql`
+  query LanchesQuery {
+    launches {
+      flight_number
+      mission_name
+      launch_date_local
+      launch_success
+    }
+  }
+`;
+
+class launches extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1 className="display-4 my-3">Launches</h1>
+        <Query query={launchQuery}>
+          {({ loading, error, data }) => {
+            if (loading) return <h4>Loading...</h4>;
+            if (error) console.log(error);
+
+            return (
+              <Fragment>
+                {data.launches.map(launch => (
+                  <LaunchItem key={launch.flight_number} launch={launch} />
+                ))}
+              </Fragment>
+            );
+          }}
+        </Query>
+      </Fragment>
+    );
+  }
+}
+
+export default launches;
