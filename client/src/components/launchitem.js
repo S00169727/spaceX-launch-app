84b16484204e22ebfc2108091bd4a7f9ae670c34
Conor Fearon <CFearon96@gmail.com>
2019-02-23 01:13:09 +0000

    Application now displays Rocket data --Added Route to launch data

--- client/src/components/launchitem.js
+++ client/src/components/launchitem.js
@@ -1,4 +1,7 @@
 import React from "react";
+import classNames from "classnames";
+import Moment from "react-moment";
+import { Link } from "react-router-dom";
 
 export default function LaunchItem({
   launch: {
@@ -10,13 +13,29 @@ export default function LaunchItem({
   }
 }) {
   return (
-    <div className="card card-body md-3">
-      <div className="col-md-9">
-        <h4>Mission: {mission_name}</h4>
-        <p>Date: {launch_date_local}</p>
-      </div>
-      <div className="col-md-3">
-        <button className="btn btn-secondary">Launch Details</button>
+    <div className="card card-body mb-3">
+      <div className="row">
+        <div className="col-md-9">
+          <h4>
+            Mission:
+            <span
+              className={classNames({
+                "text-success": launch_success,
+                "text-danger": !launch_success
+              })}
+            >
+              {mission_name}
+            </span>
+          </h4>
+          <p>
+            Date: <Moment format="YYYY-MM-DD HH:mm">{launch_date_local}</Moment>
+          </p>
+        </div>
+        <div className="col-md-3">
+          <Link to={`/launch/${flight_number}`} className="btn btn-secondary">
+            Launch Details
+          </Link>
+        </div>
       </div>
     </div>
   );
